0ff6ed850e37e190546492a2fe22ddb06d26ecd1
pietrocruciata <cruciata9748@gmail.com>
2024-04-09 14:40:47 +0200

    add: title width vite

---
+++ start-vite/src/App.vue
@@ -0,0 +1,21 @@
+<script>
+export default{
+  data(){
+    return{
+      message: 'La mia prima app con vite'
+    }
+  }
+}
+</script>
+
+<template>
+  <h1 class="center">
+    {{ message }}
+  </h1>
+</template>
+
+<style>
+.center{
+  text-align: center;
+}
+</style>
